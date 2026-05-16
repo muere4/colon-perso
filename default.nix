96921b00607f52a162f47a062a75eb32e446e8f0
muere4 <muere4@gmail.com>
2026-05-16 15:32:42 -0300

    init

---
+++ default.nix
@@ -0,0 +1,75 @@
+{ config, pkgs, lib, inputs, ... }:
+let
+  users = [ "muere" ];
+in
+{
+  fonts.packages = with pkgs; [
+    iosevka-comfy.comfy
+    nerd-fonts.fira-code
+    roboto
+  ];
+
+  environment.systemPackages = with pkgs; [
+    ripgrep
+  ];
+
+  home-manager.users = lib.genAttrs users (username: {
+    programs.emacs = {
+      enable = true;
+      package = pkgs.emacs30-pgtk;
+      extraPackages = epkgs: with epkgs; [
+        # UI
+        doom-themes
+        doom-modeline
+        nerd-icons
+        nerd-icons-dired
+
+        # Evil
+        evil
+        undo-tree
+
+        # Editor
+        envrc
+        which-key
+
+        # VC
+        magit
+
+        # LSP
+        lsp-mode
+        lsp-ui
+        yasnippet
+        quick-peek
+        dap-mode
+
+        # Lenguajes
+        rustic
+        nix-mode
+        corfu
+
+        hydra
+        eat
+
+
+        # Completion
+        vertico
+        orderless
+        consult
+        marginalia
+        flycheck
+        flycheck-inline
+
+        outshine
+        pdf-tools
+
+        projectile
+
+      ];
+    };
+
+    home.file.".config/emacs" = {
+      source = ./emacs.d;
+      recursive = true;
+    };
+  });
+}
